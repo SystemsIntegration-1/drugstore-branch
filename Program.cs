6b2d205498846c64597f167b15814ddcfb0a8ac6
gabygabylagaby <gabrielagv7656@gmail.com>
2025-03-04 22:38:48 -0400

    fix: empty space

--- Program.cs
+++ Program.cs
@@ -6,9 +6,7 @@ builder.Services.AddCors(options =>
 {
     options.AddDefaultPolicy(policy =>
     {
-        policy.WithOrigins("http://localhost:3000")
-            .AllowAnyHeader()
-            .AllowAnyMethod();
+        policy.WithOrigins("http://localhost:3000").AllowAnyHeader().AllowAnyMethod();
     });
 });
 
